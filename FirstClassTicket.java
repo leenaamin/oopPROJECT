abbf20e6af08a2fc403f7b3ff780646afaf01576
Wehad <angelwehad@gmail.com>
2024-10-23 22:17:01 +0300

    Update FirstClassTicket.java

--- FirstClassTicket.java
+++ FirstClassTicket.java
@@ -1,24 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package flightbooking;
-
 /**
  *
  * @author mayaralsaedi
- * This class extends the ticket class
- * It generates the information of a passenger that chooses to purchase a first class flight ticket
+ This class extends the Ticket class
+ It generates the information of a passenger that chooses to purchase a first class flight Ticket
  */
 public class FirstClassTicket extends Ticket {
 
     /**
-     * The seat type of the first class ticket
+     * The seat type of the first class Ticket
      */
     private String seatType;
 
     /**
-     * The price of the the first class ticket
+     * The price of the the first class Ticket
      */
     private double additionalAmenitiesFees;
 
@@ -33,7 +27,6 @@ public class FirstClassTicket extends Ticket {
     /**
      * Parameterized constructor
      * @param seatType
-     * @param basePrice
      * @param additionalAmenitiesFees
      */
     public FirstClassTicket(String seatType, double basePrice, double additionalAmenitiesFees) {
@@ -57,7 +50,7 @@ public class FirstClassTicket extends Ticket {
     }
   
     /**
-     * A settor method to set the value of the seat type attribute
+     * A setter method to set the value of the seat type attribute
      * @param seatType
      */
     public void setSeatType(String seatType) {
@@ -89,18 +82,20 @@ public class FirstClassTicket extends Ticket {
     }
 
     /**
-     * This method implements the method that is in the book ticket interface 
-     * it returns the price of the seat
-     * @return the price of the ticket
+     * This method implements the method that is in the book Ticket interface 
+ it returns the price of the seat
+     * @return the price of the Ticket
      */
     @Override
     public double GetSeatPrice() {
-        return (getBasePrice() + additionalAmenitiesFees);
+       return (getBasePrice() + additionalAmenitiesFees);
     }
-
+    
+        
+    
     /**
-     * This method overrides the abstract method in the ticket class
-     * It creates a new copy of the ticket for each passenger so it can change the information of the ticket for each passenger
+     * This method overrides the abstract method in the Ticket class
+ It creates a new copy of the Ticket for each passenger so it can change the information of the Ticket for each passenger
      * @param id
      * @param f
      * @return t
@@ -108,14 +103,14 @@ public class FirstClassTicket extends Ticket {
     @Override
     public Ticket clone(Ticket id, Flight f) {
         Ticket t = new FirstClassTicket(seatType, getBasePrice(), additionalAmenitiesFees);
-        setDetailsF(f);
-        setNumID((int) (Math.random() * (101)));
+        t.DetailsF = f;
+        t.NumID = (int) (Math.random() * (101));
         return t;
     }
   
     /**
-     * This method overrides the equals method in the ticket class
-     * It checks if two tickets are equal to each other or not
+     * This method overrides the equals method in the Ticket class
+ It checks if two tickets are equal to each other or not
      * @return true if the two objects are equal to each other and false if they are not
      */
 @Override
@@ -136,14 +131,14 @@ public boolean equals(Object obj)
 }
     
     /**
-     * This method overrides the method in the ticket class
-     * @return a string representation of the ticket's information
+     * This method overrides the method in the Ticket class
+     * @return a string representation of the Ticket's information
      */
     @Override 
     public String toString()
     {
-        return (super.toString() + "\nSeat Type: " + seatType + "\nPrice: " + GetSeatPrice() + "SAR \nIt includes lounge accces, Wi-Fi, "
-                + "Overhead TV, Prayer Area, amenity kits and Fresh meals \nBaggage Info: 3 piece luggage 32 kilos(Per Person)");
+        return (super.toString() + "\nSeat Type: " + seatType + "\nPrice: " + GetSeatPrice() + "SAR \nIt includes Wi-Fi, "
+                + "Overhead TV, Prayer Area, and Fresh meals \nBaggage Info: 1 piece luggage 7 kilos(Per Person)");
     }
 
 }
